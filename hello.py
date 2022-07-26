94b609896938701f18eafe83679037d662594847
Javi <kellerjavier13@gmail.com>
2022-07-26 20:44:50 -0300

    Números y palabras

--- hello.py
+++ hello.py
@@ -2,4 +2,11 @@
 text = "world!"
 num = 13
 
-print("Hello " + text + " " + str(num))
+print("Hello " + text + " " + str(num)) # Hello world! 13
+
+print(num) # 13
+print(num + len(text)) # 19
+print(str(num) + str(len(text))) # 136
+
+print("Las dos primeras letras del texto son: " + text[0:2]) # wo
+print("La palabra tiene " + str(len(text)) + " carácteres")
